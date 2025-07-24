b81c1ce5ad9d04e9c37181a828dbb33f26ebd6e9
Karim Pierre ZENNOUNE <karim-pierre.zennoune@laplateforme.io>
2025-07-24 14:50:48 +0200

    change some functions in jour 03/job 02 03

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -5,10 +5,20 @@ Ex. : Tu e ntnssrn edsdn etmscmelslre osl li. -->
 
 <?php
 
+function kpz_strlen($str)
+{
+    $count = 0;
+    while (isset($str[$count])) {
+        $count++;
+    }
+    return $count;
+}
+
+
 $str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.";
 
-for ($i = 0; $i < strlen($str); $i++) {
-    if ($i %2 === 0) {
+for ($i = 0; $i < kpz_strlen($str); $i++) {
+    if ($i % 2 === 0) {
         echo $str[$i];
     }
 }
--- jour03/job03/index.php
+++ jour03/job03/index.php
@@ -4,11 +4,23 @@ Créez un algorithme qui parcourt cette chaîne et affiche uniquement les voyell
 
 <?php
 
+function kpz_char_in_array($char, $array)
+{
+    $i = 0;
+    while (isset($array[$i])) {
+        if ($char == $array[$i]) {
+            return true;
+        }
+        $i++;
+    }
+    return false;
+}
+
 $str = "I'm sorry Dave I'm afraid I can't do that";
-$vowels = [ "a", "e", "i", "o", "u", "y"];
+$vowels = ["a", "e", "i", "o", "u", "y"];
 
 for ($i = 0; $i < strlen($str); $i++) {
-    if (in_array(strtolower($str[$i]), $vowels)) {
+    if (kpz_char_in_array(strtolower($str[$i]), $vowels)) {
         echo $str[$i];
     }
 }
