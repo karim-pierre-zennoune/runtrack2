86a20203ae2185b643fc1d6e7a303c305d72a948
Karim Pierre ZENNOUNE <karim-pierre.zennoune@laplateforme.io>
2025-07-30 14:55:49 +0200

    jour10 done + index.html fix

---
+++ jour10/job13/index.php
@@ -0,0 +1,71 @@
+<!-- En utilisant php, connectez-vous à la base de données “jour09”. A l’aide d’une requête
+SQL, sélectionnez récupérer le nom des salles et le nom de leur étage. Affichez le
+résultat de cette requête dans un tableau html. La première ligne de votre tableau html
+doit contenir le nom des champs. Les suivantes doivent contenir les données présentes
+dans votre base de données. -->
+
+
+<?php
+$mysqli = new mysqli("localhost", "root", "", "jour09");
+$result = $mysqli->query("SELECT etage.nom AS 'nom etage', salles.nom AS 'nom salle' FROM salles LEFT JOIN etage ON salles.id_etage = etage.id");
+$fields = $result->fetch_fields();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <style>
+        table {
+            border-collapse: collapse;
+        }
+
+        td,
+        th {
+            border: 1px solid #ddd;
+            padding: 8px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        th {
+            text-align: left;
+        }
+    </style>
+
+
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <table>
+        <thead>
+            <tr>
+                <?php foreach ($fields as $field): ?>
+                    <th> <?= $field->name ?> </th>
+                <?php endforeach; ?>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php
+            foreach ($result as $row): ?>
+                <tr>
+                    <?php foreach ($row as $key => $value): ?>
+                        <td> <?= $value ?> </td>
+                    <?php endforeach; ?>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+
+
+
+</body>
+
+</html>
